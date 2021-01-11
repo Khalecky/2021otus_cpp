9ea25394ba7753197f98b4b136308c97f886040c
roman <me@me.com>
2021-01-11 15:43:17 +0300

    try100

--- test/test_version.cpp
+++ test/test_version.cpp
@@ -5,9 +5,3 @@ TEST(Test_app, Test_Version) {
     int version = PROJECT_VERSION_PATCH;
     ASSERT_GT(version, 0);
 }
-
-int main(int argc, char *argv[])
-{
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
